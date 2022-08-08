8c3d964abbfcb27afd994549774105e9116c3cb0
Revolver0celot <rev@3six9.io>
2022-08-08 22:53:31 +0400

    updated config and deploy scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,8 +2,18 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-solhint";
 
+const ALCHEMY_API_KEY = "KEY";
+
+const GOERLI_PRIVATE_KEY = "YOUR GOERLI PRIVATE KEY";
+
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
+  },
 };
 
 export default config;
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,6 +1,18 @@
 import { ethers } from "hardhat";
 
-async function main() {}
+async function main() {
+  const Vest = await ethers.getContractFactory("Vest");
+  const RevToken = await ethers.getContractFactory("RevToken");
+  const token = await RevToken.deploy();
+  const vest = await Vest.deploy(
+    "0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199",
+    token.address
+  );
+
+  await vest.deployed();
+
+  console.log(`Contract deployred!`);
+}
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
